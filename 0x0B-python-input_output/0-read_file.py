47b550945b15fa212d85bc4395f18e1661ecce96
Amine <mouhamedamine.akchouche@gmail.com>
2024-02-07 18:07:59 +0000

    space_fix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,15 +2,6 @@
 
 
 def read_file(filename=""):
-    """Reads a text file and prints its content to stdout.
-
-    Args:
-        filename (str): The name of the file to read from.
-
-    """
-    with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(line, end="")
-
-# Example usage:
-read_file("example.txt")
+	
+    with open(filename, encoding='utf-8') as f:
+	print(f.read(), end="")
